a55a96f4923d0d876500cabd17d75e29694d9da5
David Delgado <dadch13.14@gmail.com>
2020-04-18 17:56:50 -0400

    Request.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,9 +6,9 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
         return view('test', [
-            'title' => 'Curso de PHP con Laravel - Platzi'
+            'title' => $request->query('title', 'Curso de PHP con Laravel - Platzi')
         ]);
     }
 }
